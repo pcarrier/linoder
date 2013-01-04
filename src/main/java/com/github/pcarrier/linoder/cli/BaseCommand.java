8c47811b3a137f29caafb863f00e52f78dc431ac
Pierre Carrier <pierre@spotify.com>
2013-01-04 17:13:57 +0100

    BaseCommand: support providing the API key through a config file

--- src/main/java/com/github/pcarrier/linoder/cli/BaseCommand.java
+++ src/main/java/com/github/pcarrier/linoder/cli/BaseCommand.java
@@ -3,18 +3,26 @@ package com.github.pcarrier.linoder.cli;
 import com.github.pcarrier.linoder.api.API;
 import com.github.pcarrier.linoder.api.APIBuilder;
 import com.github.pcarrier.linoder.api.exceptions.RemoteCallException;
+import com.google.common.io.Files;
 import io.airlift.command.Option;
 import io.airlift.command.OptionType;
 import lombok.Getter;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
 public abstract class BaseCommand implements Runnable {
+
     @Option(type = OptionType.GLOBAL, name = "-v", description = "Verbose mode (show detailed infos)")
     public boolean verbose;
 
-    @Option(type = OptionType.GLOBAL, name = "-d", description = "Debug mode (show API requests and replies)")
+    @Option(type = OptionType.GLOBAL, name = "-d",
+            description = "Debug mode (show API requests and replies)")
     public boolean debug;
 
-    @Option(type = OptionType.GLOBAL, name = "-k", description = "API key from https://manager.linode.com/profile/")
+    @Option(type = OptionType.GLOBAL, name = "-k",
+            description = "API key from https://manager.linode.com/profile/")
     public String api_key;
 
     @Getter
@@ -22,10 +30,15 @@ public abstract class BaseCommand implements Runnable {
 
     @Override
     public final void run() {
-        /* TODO: find the key by logging in if key not available */
         if (api_key == null) {
-            System.err.println("Unknown API key!");
-            System.exit(44);
+            final File homeKeyFile = new File(new File(System.getProperty("user.home")), ".linodekey");
+
+            try {
+                api_key = Files.readFirstLine(homeKeyFile, Charset.defaultCharset());
+            } catch (IOException e) {
+                System.err.println("Unknown API key! Please put it in " + homeKeyFile.getPath() + " or use -k");
+                System.exit(44);
+            }
         }
 
         api = new APIBuilder().withKey(api_key).withDebug(debug).build();
